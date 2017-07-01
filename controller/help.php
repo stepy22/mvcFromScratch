6c69ef934c92f07292d615169a5545dd2f4c3c24
step22 <vukasin123@gmail.com>
2017-07-01 14:05:03 +0200

    sec commit

--- controller/help.php
+++ controller/help.php
@@ -4,6 +4,8 @@ class help extends controller {
     function __construct()
     {
         parent::__construct();
+        $this->view->msg='Ovo je poruka iz helpa';
+        $this->view->render('help/index');
     }
     public function other($arg=false){
         require 'models/help_model.php';
